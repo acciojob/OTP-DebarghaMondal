0982731b0e332f5a0900dde4d25f78353cd8f08a
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-19 11:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const inputs = document.querySelectorAll('input');
-
-    inputs.forEach((input, index) => {
-        input.addEventListener('input', function(e) {
-            const currentLength = e.target.value.length;
-            if (currentLength === 1 && index < inputs.length - 1) {
-                inputs[index + 1].focus();
-            }
-        });
-
-        input.addEventListener('keydown', function(e) {
-            if (e.key === 'Backspace' && index > 0 && input.value === '') {
-                inputs[index - 1].focus();
-            }
-        });
-    });
-});
-
-
+let inputElements = document.getElementsByClassName("code");
+console.log(inputElements.length)
+for(let i=0; i< inputElements.length; i++){
+	inputElements[i].addEventListener("keyup", (event)=>{
+		let currentElement = event.target ;
+		let code = event.key.charCodeAt(0);
+		console.log(event.key)
+		if(event.key === "Backspace"){
+			let prevElement = currentElement.previousElementSibling ;
+			if(prevElement){
+				prevElement.focus();
+			}
+			return ;
+		}
+		
+		
+		
+		if(code>=48 && code<=57){
+			let nextElement = currentElement.nextElementSibling ;
+			if(nextElement){
+				nextElement.focus() ;
+			}
+			
+		}else{
+			event.target.value="" ;
+		}
+	})
+	
+}
