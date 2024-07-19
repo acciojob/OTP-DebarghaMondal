42f07a4ace3f24bb37be59603cfa8506f29c2b1a
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-19 11:46:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let inputElements = Array.from(document.querySelectorAll(".code"));
 
 inputElements.forEach((inputElement, index) => {
-    inputElement.addEventListener("keyup", (event) => {
-        let code = event.key.charCodeAt(0);
-        if (event.key === "Backspace") {
-            if (index > 0) {
-                inputElements[index - 1].focus();
-            }
-        } else if (code >= 48 && code <= 57) {
-            if (index < inputElements.length - 1) {
-                inputElements[index + 1].focus();
-            }
+    inputElement.addEventListener("keydown", (event) => {
+        if (event.key === "Backspace" && index > 0) {
+            setTimeout(() => inputElements[index - 1].focus(), 10);
+        }
+    });
+    inputElement.addEventListener("input", (event) => {
+        let code = event.data.charCodeAt(0);
+        if (code >= 48 && code <= 57 && index < inputElements.length - 1) {
+            setTimeout(() => inputElements[index + 1].focus(), 10);
         } else {
             event.target.value = "";
         }
